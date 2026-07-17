e3daa7f1f587124c9474c3ca7fa872303a462188
MiekeDams <info@zinergi.nl>
2026-07-17 15:29:49 +0200

    Laat videospeler starten binnen browserbeleid

--- story.js
+++ story.js
@@ -2,7 +2,15 @@ const video=document.getElementById('miekeVideo');
 const play=document.getElementById('videoPlay');
 const wrap=document.querySelector('.video-wrap');
 play.addEventListener('click',async()=>{
-  try{await video.play();wrap.classList.add('playing')}catch(e){video.controls=true}
+  try{
+    video.muted=true;
+    await video.play();
+    wrap.classList.add('playing');
+    setTimeout(()=>{video.muted=false;video.volume=.9},180);
+  }catch(e){
+    video.controls=true;
+    window.location.href=video.querySelector('source').src;
+  }
 });
 video.addEventListener('play',()=>wrap.classList.add('playing'));
 video.addEventListener('ended',()=>wrap.classList.remove('playing'));
